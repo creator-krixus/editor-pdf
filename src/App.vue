2f66da717b00bd8eea08c844f29157ac71eb09b7
wilsonRueda <wilson.rueda@onegroupitservices.com>
2024-09-19 23:08:31 -0500

    Feat: my editor pdf

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import WebViewer from "./components/WebViewer.vue"
+</script>
+
+<template>
+  <HelloWorld />
+  <WebViewer
+    initialDoc="https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf" />
+</template>
+
+<style>
+</style>
---
+++ src/components/WebViewer.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, onMounted } from "vue"
+import WebViewer from "@pdftron/webviewer"
+const props = defineProps({
+    initialDoc: String,
+})
+const viewerDiv = ref(null)
+onMounted(() => {
+    const path = `${import.meta.env.BASE_URL}lib`
+    WebViewer({ path, initialDoc: props.initialDoc }, viewerDiv.value)
+})
+</script>
+<template>
+    <div id="webViewer" ref="viewerDiv">
+
+    </div>
+</template>
+<style>
+#webViewer {
+    width: 1280px;
+    height: 200vh;
+}
+</style>
